2c9c0c34702c4269be9877b2cecfefa68df10f25
Heidy Pastor <heidypastor@gmail.com>
2022-11-25 01:33:54 -0500

    watch-list

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,6 +3,11 @@
 // ./bin/rails generate stimulus controllerName
 
 import { application } from "./application"
+import TomSelectController from "./tom_select_controller.js"
+application.register("tom-select", TomSelectController)
+
+import StarRatingController from "./star_rating_controller.js"
+application.register("star-rating", StarRatingController)
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
